98b05aecb97e26208f3bd201175e2f45d05f5d1f
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-06-03 23:54:58 +0530

    Time: 10 ms (74.39%), Space: 14 MB (51.11%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int newColor) {
+        if(grid[sr][sc]==newColor)
+            return grid;
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        int currColor=grid[sr][sc];
+        
+        DFS(sr,sc,m,n,currColor,newColor,grid);
+        return grid;
+        
+        
+    }
+    
+    void DFS(int  currRow,int currCol,int m,int n ,int currColor,int newColor,vector<vector<int>>& grid)
+    {
+        
+        
+        
+        
+        if(currRow <0 || currRow >=m || currCol <0 || currCol >=n || grid[currRow][currCol]!=currColor) 
+        {
+            return ;
+        }
+        
+        grid[currRow][currCol]=newColor;
+        
+        
+        DFS(currRow-1,currCol,m,n,currColor,newColor,grid);
+        DFS(currRow+1,currCol,m,n,currColor,newColor,grid);
+        DFS(currRow,currCol-1,m,n,currColor,newColor,grid);
+        DFS(currRow,currCol+1,m,n,currColor,newColor,grid);
+        
+        return ;
+        
+        
+    }
+};
