3b3103461e0ff57fe16b467d6baf14dd8a6419f2
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-06-09 22:49:00 +0530

    Time: 22 ms (12.62%), Space: 14 MB (81.06%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,38 +1,40 @@
 class Solution {
-    
-    vector<vector<int>>dir{{1,0},{-1,0},{0,1},{0,-1}};
-    
-    
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int newColor) {
+        if(grid[sr][sc]==newColor)
+            return grid;
         int m=grid.size();
         int n=grid[0].size();
-       vector<vector<bool>>visited(m,vector<bool>(n,false));
         
         int currColor=grid[sr][sc];
-        Replace(sr,sc,currColor,newColor,m,n,visited,grid);
         
+        DFS(sr,sc,m,n,currColor,newColor,grid);
         return grid;
         
         
     }
     
-    void Replace(int currRow,int currCol,int currColor,int newColor ,int m,int n, vector<vector<bool>>&visited,vector<vector<int>>& grid)
+    void DFS(int  currRow,int currCol,int m,int n ,int currColor,int newColor,vector<vector<int>>& grid)
     {
-        if(currRow < 0 || currCol <0 || currRow >=m || currCol >=n || grid[currRow][currCol]!=currColor || visited[currRow][currCol]==true)
+        
+        
+        
+        
+        if(currRow <0 || currRow >=m || currCol <0 || currCol >=n || grid[currRow][currCol]!=currColor) 
+        {
             return ;
-            grid[currRow][currCol]=newColor;
-            visited[currRow][currCol]=true;
-            for(auto eachDir:dir)
-            {
-                int nextRow=currRow+eachDir[0];
-                int nextCol=currCol+eachDir[1];
-                Replace(nextRow,nextCol,currColor,newColor,m,n,visited,grid);
-                
-                
-            }
         }
         
-    
-    
+        grid[currRow][currCol]=newColor;
+        
+        
+        DFS(currRow-1,currCol,m,n,currColor,newColor,grid);
+        DFS(currRow+1,currCol,m,n,currColor,newColor,grid);
+        DFS(currRow,currCol-1,m,n,currColor,newColor,grid);
+        DFS(currRow,currCol+1,m,n,currColor,newColor,grid);
+        
+        // return ;
+        
+        
+    }
 };
