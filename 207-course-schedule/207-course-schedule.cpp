cfd438c4d7d397c96e3e3d1da701c804406acb0e
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-07-04 18:52:05 +0530

    Time: 50 ms (18.16%), Space: 17.6 MB (5.31%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -2,74 +2,71 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>graph=createGraph(numCourses,prerequisites);
-        vector<int>inDegree(numCourses,0);
         vector<bool>visited(numCourses,false);
-        vector<int>ans;
-        queue<int>que;
-        for(int i=0;i<numCourses;i++)
-        {
-            vector<int>neigh=graph[i];
-            
-           for(auto eachNeigh:neigh)
-           {
-               inDegree[eachNeigh]++;
-               
-           }
-        }
+        unordered_set<int>stt;
         
         for(int i=0;i<numCourses;i++)
         {
-            cout<<inDegree[i]<<"   ";
-            if(inDegree[i]==0)
+            if(visited[i]==false)
             {
-                que.push(i);
-            }
-                
-        }
-            
-        while(!que.empty())
-        {
-            int currNode=que.front();
-            que.pop();
-            if(visited[currNode])
-                continue;
-            visited[currNode]=true;
-            ans.push_back(currNode);
-            vector<int>neigh=graph[currNode];
-            for(auto eachNeigh:neigh)
-            {
-                inDegree[eachNeigh]--;
-                if(inDegree[eachNeigh]==0)
-                    que.push(eachNeigh);
+                if(DFS(i,visited,stt,graph))
+                    return false;
             }
         }
-            cout<<endl<<ans.size();
-            return ( ans.size()==numCourses);
-               
-            
+        
+        return true;
     }
     
-    vector<vector<int>>createGraph(int numCourses, vector<vector<int>>& prerequisites)
-        
+    bool DFS(int currVertex,vector<bool>&visited,unordered_set<int>&stt,vector<vector<int>>&graph)
     {
-         vector<vector<int>>graph;
+        visited[currVertex]=true;
+        stt.insert(currVertex);
+        
+        vector<int>neigh=graph[currVertex];
+        
+        for(auto eachNeigh:neigh)
+        {
+            if(visited[eachNeigh]==false)
+            {
+                if(DFS(eachNeigh,visited,stt,graph))
+                    return true;
+            }
+            else {
+                if(stt.find(eachNeigh)!=stt.end())
+                    return true;
+            }
+            
+        }
+        
+        stt.erase(currVertex);
+        return false;
+        
+        
+    }
+    
+    
+    vector<vector<int>>createGraph(int numCourses,vector<vector<int>>& prerequisites)
+    {
+        vector<vector<int>>graph;
         
         for(int i=0;i<numCourses;i++)
         {
-            vector<int>temp;
-            graph.push_back(temp);
+            vector<int>temp1;
+            graph.push_back(temp1);
         }
         
-        for(auto eachArray:prerequisites)
+        for(auto temp22:prerequisites)
         {
-            int a=eachArray[0];
-            int b =eachArray[1];
+            int a=temp22[0];
+            int b=temp22[1];
             vector<int>temp;
             temp=graph[b];
+              
             temp.push_back(a);
             graph[b]=temp;
         }
         
         return graph;
+     
     }
 };
