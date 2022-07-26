e3e041392d9f59c1fa1e13bc2aa3b200b33e1193
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-07-26 15:32:22 +0530

    Time: 79 ms (7.68%), Space: 30.5 MB (5.02%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -3,37 +3,55 @@ class Solution
     public:
         bool isBipartite(vector<vector < int>> &graph)
         {
-            int v = graph.size();
 
-            vector<int> colors(v, -1);
+            int v = graph.size();
+            vector<int> nums(v, -1);
 
             for (int i = 0; i < v; i++)
             {
-                if (colors[i] == -1) {
-                    if(!isCycleEven(graph,v,i,0,colors))
-                        return false;
-                }
+                if (nums[i] != -1)
+                    continue;
+
+                if (!BFS(i, 0, nums, graph))
+                    return false;
             }
             return true;
         }
 
-    bool isCycleEven(vector<vector < int>> &graph, int v, int currVertex, int currColor, vector< int > &colors)
+    bool BFS(int Vertex, int Color, vector<int> &nums, vector<vector< int>> &graph)
+
     {
-        if (colors[currVertex] != -1)
+        queue<pair<int, int>> que;
 
-            return (colors[currVertex] == currColor);
+        que.push({ Vertex,1});
+        nums[Vertex]=1;
+        while (!que.empty())
+        {
+            auto currPair = que.front();
+            que.pop();
 
-    colors[currVertex] = currColor;
-    vector<int> neigh = graph[currVertex];
-    for (auto eachNeigh: neigh)
-    {
-       if (!isCycleEven(graph, v, eachNeigh, 1 - currColor, colors))
-        return false;
+            int currVertex = currPair.first;
+            int currColor = currPair.second;
 
+            if (nums[currVertex] != -1)
+            {
+                if (nums[currVertex] != currColor)
+                    return false;
+                  
+            }
+           else
+           {
+                nums[currVertex] = currColor;
+           }
+            vector<int> neigh = graph[currVertex];
+          
+            for (auto eachNeigh: neigh)
+            {
+                if(nums[eachNeigh]==-1)
+                que.push({ eachNeigh,1 - currColor });
+            }
+          
+        }
+          return true;
     }
-        return true;
-
-   
-}
-
 };
