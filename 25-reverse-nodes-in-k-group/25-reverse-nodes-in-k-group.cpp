d1011b38ae50b003f313601f1a6f46f6337065ba
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-08-09 08:39:13 +0530

    Time: 28 ms (39.27%), Space: 11.6 MB (29.96%) - LeetHub

---
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,75 @@
+/**
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *int val;
+ *ListNode * next;
+ *ListNode() : val(0), next(nullptr) {}
+ *ListNode(int x) : val(x), next(nullptr) {}
+ *ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
+ */
+class Solution
+{
+    public:
+        ListNode* reverseKGroup(ListNode *head, int k)
+        {
+            if (k == 1 || head == NULL)
+                return head;
+
+            bool check = validate(head, k);
+           	
+            if (check)
+            {
+
+                ListNode *curr = head;
+                int currLength = 1;
+
+                while (curr->next != NULL && currLength < k)
+                {
+                    currLength++;
+                    curr = curr->next;
+                }
+                cout << currLength << ",";
+                ListNode *tempNode = curr->next;
+                curr->next = NULL;
+
+                ListNode *reversedList = reverseList(head);
+                head->next = reverseKGroup(tempNode, k);
+                return reversedList;
+            }
+            else
+            {
+                return head;
+            }
+        }
+
+    ListNode* reverseList(ListNode *head)
+    {
+        ListNode *prev = NULL;
+        ListNode *currNode = head;
+
+        while (currNode != NULL)
+        {
+            ListNode *tempNode = currNode->next;
+            currNode->next = prev;
+            prev = currNode;
+            currNode = tempNode;
+        }
+        return prev;
+    }
+
+    bool validate(ListNode *head, int k)
+    {
+        ListNode *curr = head;
+        int currLength = 1;
+
+        while (curr->next != NULL && currLength < k)
+        {
+            currLength++;
+            curr = curr->next;
+        }
+        if (currLength == k)
+            return true;
+        return false;
+    }
+};
