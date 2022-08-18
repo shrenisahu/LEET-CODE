da4253593f3389f6e42173822a3421882a89b0e5
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-08-18 14:35:38 +0530

    Time: 269 ms (64.09%), Space: 98.6 MB (36.80%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -3,50 +3,41 @@ class Solution
     public:
         int minOperations(vector<int> &nums, int x)
         {
+            int mod=1000000007;
 
             int totalSum = 0;
             for (auto eachNum: nums)
             {
-                totalSum += eachNum;
+                totalSum +=eachNum;
             }
 
             int target = totalSum - x;
             if (target == 0)
-                return nums.size();
-            int ans = largestSubArray(nums, target);
-            cout << target << "  " << ans;
-            if (ans == 0)
+            return nums.size();
+            int maxL = -1;
+            int left = 0;
+            int n = nums.size();
+            int currSum = 0;
+            // cout<<"tar"<<target<<endl;
+            for (int right = 0; right < n; right++)
+            {
+                currSum += nums[right];
+               
+               
+                while (left <= right && currSum > target)
+                {
+                    currSum -= nums[left];
+                    left+=1;
+                }
+                 if (currSum == target)
+                {
+                    maxL = max(maxL, right - left + 1);
+                }
+
+            }
+            // cout<<maxL<<" ";
+            if(maxL==-1)
                 return -1;
-            return nums.size() - ans;
+            return n - maxL;
         }
-
-    int largestSubArray(vector<int> &nums, int target)
-    {
-
-        int Sum = 0;
-        int maxLength = 0;
-        unordered_map<int, int> mpp;
-        mpp[0] = -1;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            Sum += nums[i];
-
-            if (mpp.find(Sum) == mpp.end())
-            {
-                mpp[Sum] = i;
-            }
-
-            if (mpp.find(Sum - target) != mpp.end())
-            {
-                maxLength = max(maxLength, i - mpp[Sum - target]);
-                cout << maxLength << "  ";
-            }
-        }
-
-        for (auto temp: mpp)
-        {
-           	// cout<<temp.first<<"  "<<temp.second<<endl;
-        }
-        return maxLength;
-    }
 };
