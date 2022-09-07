89f400048545bdf44854e57145ae658a43234f41
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-07 20:05:43 +0530

    Time: 43 ms (45.71%), Space: 27.6 MB (91.56%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -25,8 +25,8 @@ class Solution
         if (root == NULL)
             return 0;
 
-        int leftAns = max(0, Solve(root->left));
-        int rightAns = max(0, Solve(root->right));
+        int leftAns =  Solve(root->left);
+        int rightAns = Solve(root->right);
         if (leftAns < 0)
             leftAns = 0;
         if (rightAns < 0)
