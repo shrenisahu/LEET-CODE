3729b09d0ee976a4386a710568427eb6ca407107
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-08 10:39:24 +0530

    Time: 16 ms (61.26%), Space: 21.4 MB (38.68%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -14,30 +14,23 @@ class Solution
     public:
         bool hasPathSum(TreeNode *root, int targetSum)
         {
-           if(root==NULL)
-               return false;
+            if (root == NULL)
+                return false;
             return Solve(root, 0, targetSum);
         }
 
     bool Solve(TreeNode *root, int currSum, int targetSum)
     {
+        if (root == NULL)
+            return false;
 
-        if (root->left == NULL && root->right == NULL)
+        if (root->left == NULL && root->right == NULL && currSum + root->val == targetSum)
         {
-            currSum += root->val;
-            if (currSum == targetSum)
-            {
-                cout << "here";
-                return true;
-            }
-        }
-        bool leftAns = false;
-        bool rightAns = false;
-        if (root->left)
-             leftAns = Solve(root->left, currSum + root->val, targetSum);
-        if (root->right)
-             rightAns = Solve(root->right, currSum + root->val, targetSum);
 
-        return leftAns || rightAns;
+            cout << "here";
+            return true;
+        }
+
+        return Solve(root->left, currSum + root->val, targetSum) || Solve(root->right, currSum + root->val, targetSum);
     }
 };
