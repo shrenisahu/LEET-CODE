12f307ecf2d87b07de5c5e974919e92f7183fce3
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-08 11:17:12 +0530

    Time: 6 ms (30.73%), Space: 9.3 MB (47.20%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -1,31 +1,34 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        int sumNumbers(TreeNode *root) {
-            int currSum=0;
-            if(root==NULL) return 0;
-            Solve(root,0,currSum);
+        int sumNumbers(TreeNode *root)
+        {
+            int currSum = 0;
+            int ans = 0;
+            if (root == NULL) return 0;
+            Solve(root, ans, currSum);
             return currSum;
         }
 
-    void Solve(TreeNode *root, int currStr, int &currSum)
+    void Solve(TreeNode *root, int &currStr, int &currSum)
     {
 
         if (root->left == NULL && root->right == NULL)
         {
             currStr = currStr *10 + root->val;
             currSum += currStr;
+            currStr = currStr / 10;
             return;
         }
         currStr = currStr *10 + root->val;
@@ -33,5 +36,6 @@ class Solution
             Solve(root->left, currStr, currSum);
         if (root->right)
             Solve(root->right, currStr, currSum);
+        currStr = currStr / 10;
     }
 };
