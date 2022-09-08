5d5c4583a794a2b0d8bbce9ab1202c326bcebecc
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-09 00:03:08 +0530

    Time: 3 ms (60.90%), Space: 8.3 MB (75.10%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,43 +1,30 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        
-        vector<int>ans;
-        stack<TreeNode *>st;
-        TreeNode *currNode=root;
-        
-        while(true)
-        
-        {
-            if(currNode !=NULL)
-            {
-                st.push(currNode);
-                currNode=currNode->left;
-            }
-            else
-            {
-                if(st.empty()) break;
-                
-                currNode=st.top();
-                st.pop();
-                ans.push_back(currNode->val);
-                currNode=currNode->right;
-            }
-                
+class Solution
+{
+    
+    public:
+    vector<int>ans;
+        vector<int> inorderTraversal(TreeNode *root) {
             
-            
+            Traverse(root);
+            return ans;
         }
-        return ans;
+
+    void Traverse(TreeNode *root)
+    {
+        if (root == NULL) return;
+        Traverse(root->left);
+        ans.push_back(root->val);
+        Traverse(root->right);
     }
 };
