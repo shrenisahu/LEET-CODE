32bda8eb57ece515867b3f68352ffb309b71feef
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-09 13:39:51 +0530

    Time: 679 ms (13.65%), Space: 85.6 MB (17.86%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,31 +1,61 @@
-class Solution
-{
-    public:
-        bool isPossible(vector<int> &nums, int index, int targetSum, int n, unordered_map<string, bool> &mymap)
-        {
-            if (targetSum == 0) return true;
-            if (index >= n) return false;
-            string key = to_string(index) + "-" + to_string(targetSum);
-            if (mymap.find(key) != mymap.end()) return mymap[key];
-            bool consider = false;
-            if (nums[index] <= targetSum)
-            {
-                consider = isPossible(nums, index + 1, targetSum - nums[index], n, mymap);
-            }
-            if (consider)
-            {
-                // mymap[key] = consider;
-                return true;
-            }
-            bool notConsider = isPossible(nums, index + 1, targetSum, n, mymap);
-            mymap[key] = consider || notConsider;
-            return consider || notConsider;
-        }
-    bool canPartition(vector<int> &nums)
+class Solution {
+public:
+    bool fUtils(vector<int> &nums, int idx, int target, vector<vector<int>> &dp)
     {
-        int totalsum = accumulate(nums.begin(), nums.end(), 0);
-        if (totalsum % 2 != 0) return false;
-        unordered_map<string, bool> mymap;
-        return isPossible(nums, 0, totalsum / 2, nums.size(), mymap);
+	//boundary cases
+        if(idx >= nums.size() || target < 0)
+        {
+            return false;
+        }
+		//success case
+        if(target == 0)
+        {
+            return true;
+        }
+		//memoisation used
+        if(dp[idx][target] != -1)
+        {
+            return dp[idx][target];
+        }
+        
+        //pick it
+        bool pick = false;
+        if(nums[idx] <= target)
+        {
+            pick = fUtils(nums, idx + 1, target - nums[idx], dp);
+        }
+        //ignore it
+        bool npick = fUtils(nums, idx + 1, target, dp);
+        
+        return dp[idx][target] = pick || npick;
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if(n <= 1)
+        {
+            return false;
+        }
+        
+        int sum = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            sum += nums[i];
+        }
+        
+        //if sum is odd it cant be formed
+        if(sum % 2 > 0)
+        {
+            return false;
+        }
+        
+        int val = sum / 2;
+        
+        vector<vector<int>> dp(n+1, vector<int>((sum/2) + 1, -1));//at a particular index and target can i reach or not
+        
+        return fUtils(nums, 0, val, dp);
     }
 };
