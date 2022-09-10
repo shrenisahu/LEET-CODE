05c80b6ebb4e74ddd4a65605ea5eb31bcf72d19b
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-10 09:42:45 +0530

    Time: 360 ms (5.02%), Space: 93.3 MB (51.70%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,40 +1,19 @@
-class Solution
-{
-    public:
-        int maxProfit(vector<int> &prices)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        
+        int maxProfit=0;
+        int maxTillNow=INT_MIN;
+        
+        for(int i=n-2;i>=0;i--)
         {
-            int n = prices.size() + 1;
-            vector<vector<vector< int>>> dp(n, vector<vector < int>> (2, vector<int> (2, -1)));
-            int ans = Profit(0, 1, 1, prices, dp);
-            if (ans < 0)
-                return 0;
-
-            return ans;
+            maxTillNow=max(maxTillNow,prices[i+1]);
+            maxProfit=max(maxProfit,maxTillNow-prices[i]);
         }
-
-    int Profit(int currIndex, int canBuy, int transactionCount, vector<int> &prices, vector<vector< vector< int>>> &dp)
-
-    {
-
-        if (currIndex >= prices.size() && transactionCount > 0)
-            return -10000;
-
-        if (transactionCount == 0 || currIndex >= prices.size())
-            return 0;
-        if (dp[currIndex][canBuy][transactionCount] != -1)
-            return dp[currIndex][canBuy][transactionCount];
-        if (canBuy == 1)
-        {
-            int idle = Profit(currIndex + 1, canBuy, transactionCount, prices, dp);
-            int buy = -prices[currIndex] + Profit(currIndex + 1, 1 - canBuy, transactionCount, prices, dp);
-
-            return dp[currIndex][canBuy][transactionCount] = max(idle, buy);
-        }
-        else
-        {
-            int idle = Profit(currIndex + 1, canBuy, transactionCount, prices, dp);
-            int sell = prices[currIndex] + Profit(currIndex + 1, 1 - canBuy, transactionCount - 1, prices, dp);
-            return dp[currIndex][canBuy][transactionCount] = max(idle, sell);
-        }
+        
+        return maxProfit;
+        
+        
     }
 };
