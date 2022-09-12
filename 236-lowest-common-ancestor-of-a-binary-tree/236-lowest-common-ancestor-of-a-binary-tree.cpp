e237858ef77cd5555ee9c13231d6c6d1f9745a74
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-12 19:43:52 +0530

    Time: 39 ms (11.74%), Space: 16.2 MB (13.01%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,43 +1,44 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  *};
  */
 class Solution
 {
+    set<TreeNode*> stt;
     public:
         TreeNode* lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
         {
-
-           return lowestCommonAncestor2(root,p,q);
+            stt.insert(p);
+            stt.insert(q);
+            return lowestCommonAncestor2(root, p, q);
         }
-    
-     TreeNode* lowestCommonAncestor2(TreeNode *root, TreeNode *p, TreeNode *q)
-        {
 
-            if (root == NULL)
-                return NULL;
+    TreeNode* lowestCommonAncestor2(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
 
-            if (root->val == p->val || root->val == q->val)
-                return root;
-
-            TreeNode *leftAns = lowestCommonAncestor2(root->left, p, q);
-            TreeNode *rightAns = lowestCommonAncestor2(root->right, p, q);
-
-            if (leftAns != NULL && rightAns != NULL)
-                return root;
-
-            if (leftAns != NULL)
-                return leftAns;
-
-            if ( rightAns != NULL)
-                return rightAns;
-            
-            
+        if (root == NULL)
             return NULL;
-        }
+
+        if(stt.find(root)!=stt.end())
+            return root;
+
+        TreeNode *leftAns = lowestCommonAncestor2(root->left, p, q);
+        TreeNode *rightAns = lowestCommonAncestor2(root->right, p, q);
+
+        if (leftAns != NULL && rightAns != NULL)
+            return root;
+
+        if (leftAns != NULL)
+            return leftAns;
+
+        if (rightAns != NULL)
+            return rightAns;
+
+        return NULL;
+    }
 };
