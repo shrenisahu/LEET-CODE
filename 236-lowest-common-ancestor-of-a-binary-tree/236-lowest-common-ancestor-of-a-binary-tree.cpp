4289e50eef04e8fa271cd92ff0be8e611855a57c
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-12 09:51:26 +0530

    Time: 17 ms (87.82%), Space: 14.3 MB (23.16%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,42 +1,43 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *};
  */
-class Solution {
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return LCA(root,p,q);
-        
-    }
-    TreeNode *LCA(TreeNode *root,TreeNode *p,TreeNode *q)
-    {
-        if(root==NULL)
-            return NULL;
-        
-        if(root->val==p->val || root->val ==q->val)
+class Solution
+{
+    public:
+        TreeNode* lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
         {
-            return root;
+
+           return lowestCommonAncestor2(root,p,q);
         }
-        
-        TreeNode *leftAns=LCA(root->left,p,q);
-        TreeNode * rightAns=LCA(root->right,p,q);
-        
-        if(leftAns!=NULL && rightAns!=NULL)
-            return root;
-        
-        if(leftAns!=NULL)
-            return leftAns;
-        
-        if(rightAns!=NULL)
-            return rightAns;
-        
-       
-        
-        return NULL;
-    }
+    
+     TreeNode* lowestCommonAncestor2(TreeNode *root, TreeNode *p, TreeNode *q)
+        {
+
+            if (root == NULL)
+                return NULL;
+
+            if (root->val == p->val || root->val == q->val)
+                return root;
+
+            TreeNode *leftAns = lowestCommonAncestor2(root->left, p, q);
+            TreeNode *rightAns = lowestCommonAncestor2(root->right, p, q);
+
+            if (leftAns != NULL && rightAns != NULL)
+                return root;
+
+            if (leftAns != NULL)
+                return leftAns;
+
+            if ( rightAns != NULL)
+                return rightAns;
+            
+            
+            return NULL;
+        }
 };
