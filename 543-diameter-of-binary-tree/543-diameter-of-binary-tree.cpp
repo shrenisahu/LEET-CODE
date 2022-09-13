fa211bec2b6bc8832908e4839d1f68b4acf78c25
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-13 20:11:44 +0530

    Time: 19 ms (47.11%), Space: 20.2 MB (64.94%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,48 +1,31 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *int val;
- *TreeNode * left;
- *TreeNode * right;
- *TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-
-//TC:O(N*N)  worst case 
-class Solution
-{
-    int maxElem = 0;
-    public:
-        int diameterOfBinaryTree(TreeNode *root)
-        {
-            if (root == NULL)
-                return 0;
-            int ans= findHeight(root);
-            return maxElem;
-        }
-
-    void Solve(TreeNode *root)
-    {
-        if (root == NULL)
-            return;
-        int lh = findHeight(root->left);
-        int rh = findHeight(root->right);
-        maxElem = max(maxElem, lh + rh);
-
-        Solve(root->left);
-        Solve(root->right);
+class Solution {
+public:
+    int maxHeight=0;
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans=findHeight(root);
+        return maxHeight;
     }
-
+    
     int findHeight(TreeNode *root)
     {
-        if (root == NULL)
+        if(root==NULL)
             return 0;
-        int lh = findHeight(root->left);
-        int rh = findHeight(root->right);
-        maxElem = max(maxElem, lh + rh);
-
-        return 1 + max(lh, rh);
+        
+        int leftHeight=findHeight(root->left);
+        int rightHeight=findHeight(root->right);
+        maxHeight=max(maxHeight,leftHeight+rightHeight);
+        
+        return 1+max(leftHeight,rightHeight);
     }
 };
