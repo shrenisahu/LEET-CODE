09a0c73780f2a1a3cd603a25efc74f6ed13afdfd
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-14 10:13:52 +0530

    Time: 290 ms (19.45%), Space: 86.4 MB (9.20%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -1,39 +1,38 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
         int ans = 0;
+
     int goodNodes(TreeNode *root)
     {
-        priority_queue<int> que;
 
         if (root == NULL)
             return 0;
 
-        preOrder(root, que);
+        int maxValue = INT_MIN;
+        preOrder(root, maxValue);
         return ans;
     }
 
-    void preOrder(TreeNode *root, priority_queue<int> que)
+    void preOrder(TreeNode *root, int maxValue)
     {
         if (root == NULL)
             return;
-
-        que.push(root->val);
-        if (que.top() <= root->val)
+        maxValue = max(maxValue, root->val);
+        if (root->val >= maxValue)
             ans++;
-
-        preOrder(root->left, que);
-        preOrder(root->right, que);
+        preOrder(root->left, maxValue);
+        preOrder(root->right, maxValue);
     }
 };
