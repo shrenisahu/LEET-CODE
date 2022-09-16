3d23b24d8732bb037bb1ad435ab0ecdcd3d4cf6a
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-16 21:23:34 +0530

    Time: 11 ms (76.10%), Space: 19 MB (14.12%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -1,22 +1,39 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-        if(root==NULL) return 0;
-        int lh=1+maxDepth(root->left);
-        int rh=1+maxDepth(root->right);
-        
-        return max(lh,rh);
-        
+class Solution
+{
+    int maxHeight=1;
+    public:
+        int maxDepth(TreeNode *root)
+        {
+            int currHeight = 1;
+            if (root == NULL)
+                return 0;
+            Traverse(root, currHeight);
+            return maxHeight;
+
+        }
+
+    void Traverse(TreeNode *root, int currHeight)
+    {
+        if (root == NULL)
+            return;
+
+        maxHeight = max(maxHeight, currHeight);
+        if (root->left)
+            Traverse(root->left, currHeight + 1);
+
+        if (root->right)
+            Traverse(root->right, currHeight + 1);
+        return;
     }
 };
