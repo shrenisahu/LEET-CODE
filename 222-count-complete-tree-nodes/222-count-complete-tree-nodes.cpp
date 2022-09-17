4c4a759e62e0b6c42048afe8096a2aaed31c01de
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-17 10:37:19 +0530

    Time: 63 ms (33.33%), Space: 30.9 MB (20.29%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -1,62 +1,60 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
-    int leftHeight = 0;
-    int countLeaf = 0;
-    TreeNode *leftMostNode = NULL;
-    int mainIndex = 0;
 
     public:
         int countNodes(TreeNode *root)
         {
+            int leftHeight = 0, rightheight = 0;
             if (root == NULL)
                 return 0;
 
-            findLeftHeight(root);
-            Traverse(root, 1, 1);
-            cout << mainIndex << endl;
-            return mainIndex;
+            return Traverse(root);
         }
-    void Traverse(TreeNode *root, int currLevel, int currIndex)
+
+    int Traverse(TreeNode *root)
+    {
+        int height = 0;
+
+        int leftH = findLeftHeight(root);
+        int RightH = findRightHeight(root);
+        if (leftH == RightH)
+            return pow(2, leftH) - 1;
+
+        return 1 + Traverse(root->left) + Traverse(root->right);
+    }
+
+    int findLeftHeight(TreeNode *root)
     {
 
         if (root == NULL)
-            return;
-
-        if (currLevel == leftHeight)
         {
 
-            mainIndex = max(mainIndex,currIndex);
-            // cout << "mainIndex" << mainIndex << "  ";
-            return;
+            return 0;
         }
-        if (root->right)
-            Traverse(root->right, currLevel + 1, 2 *currIndex + 1);
-        if (root->left)
-            Traverse(root->left, currLevel + 1, 2 *currIndex + 0);
+
+        return 1 + findLeftHeight(root->left);
     }
 
-    void findLeftHeight(TreeNode *root)
+    int findRightHeight(TreeNode *root)
     {
 
-        leftHeight++;
-        if (root->left == NULL && root->right == NULL)
+        if (root == NULL)
         {
-            leftMostNode = root;
-            return;
-        }
-        findLeftHeight(root->left);
 
-        return;
+            return 0;
+        }
+
+        return 1 + findRightHeight(root->right);
     }
 };
