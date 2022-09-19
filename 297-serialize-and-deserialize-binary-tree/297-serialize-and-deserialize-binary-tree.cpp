404099692e59f58a1f5b831d172394cbf0a4429e
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-19 17:51:33 +0530

    Time: 998 ms (5.01%), Space: 728.2 MB (5.01%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -14,77 +14,63 @@ class Codec
        	// Encodes a tree to a single string.
         string serialize(TreeNode *root)
         {
-            string nodesIndex = "";
 
-            if (root == NULL)
-                return nodesIndex;
-            queue<TreeNode*> que;
+            string data = "";
+            Preoder(root, data);
+            cout << data;
 
-            que.push(root);
-            while (!que.empty())
-            {
-                TreeNode *currNode = que.front();
-                que.pop();
-                if (currNode == NULL)
-                {
-                    nodesIndex += "#,";
-                }
-                else
-                    nodesIndex += to_string(currNode->val) + ',';
-
-                if (currNode != NULL)
-                {
-                    que.push(currNode->left);
-                    que.push(currNode->right);
-                }
-            }
-            cout << nodesIndex << endl;
-            return nodesIndex;
+            return data;
         }
 
+    void Preoder(TreeNode *root, string &data)
+    {
+
+        if (root == NULL)
+        {
+            data += "#,";
+            return;
+        }
+        data += to_string(root->val) + ',';
+        Preoder(root->left, data);
+        Preoder(root->right, data);
+        return;
+    }
    	// Decodes your encoded data to tree.
     TreeNode* deserialize(string data)
     {
+
         if (data.size() == 0)
             return NULL;
-        stringstream obj; //    stringstream obj(data);
-        obj << data;
-        string str;
-        getline(obj, str, ',');
+        int index = 0;
+        return constructTree(data, index);
+    }
 
-        if (str == "#")
-            return NULL;
-
-        queue<TreeNode*> que;
-        TreeNode *root = new TreeNode(stoi(str));
-        que.push(root);
-        while (!que.empty())
+    int getValue(string data, int &i)
+    {
+        string res = "";
+        while (data[i] != ',')
         {
-
-            TreeNode *currNode = que.front();
-            que.pop();
-            string left;
-            getline(obj, left, ',');
-            if (left == "#")
-                currNode->left == NULL;
-            else
-            {
-                TreeNode *leftNode = new TreeNode(stoi(left));
-                currNode->left = leftNode;
-                que.push(leftNode);
-            }
-
-            string right;
-            getline(obj, right, ',');
-            if (right == "#")
-                currNode->right == NULL;
-            else
-            {
-                TreeNode *rightNode = new TreeNode(stoi(right));
-                currNode->right = rightNode;
-                que.push(rightNode);
-            }
+            res += data[i];
+            i++;
         }
+        i++;
+        return stoi(res);
+    }
+
+    TreeNode* constructTree(string data, int &index)
+    {
+        if (data[index] == '#')
+        {
+            index+=2;
+            return NULL;
+        }
+
+        int currVal = getValue(data, index);
+
+        TreeNode *root = new TreeNode(currVal);
+        root->left = constructTree(data, index);
+        root->right = constructTree(data, index);
+
         return root;
     }
 };
