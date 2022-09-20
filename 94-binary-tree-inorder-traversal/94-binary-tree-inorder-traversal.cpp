71c70a7716bd104350561b540a96419e5996cc7f
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-20 16:44:42 +0530

    Time: 6 ms (21.73%), Space: 8.3 MB (75.10%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,30 +1,52 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
-    
     public:
-    vector<int>ans;
-        vector<int> inorderTraversal(TreeNode *root) {
-            
-            Traverse(root);
+        vector<int> inorderTraversal(TreeNode *root)
+        {
+            vector<int> ans;
+
+            TreeNode *curr = root;
+
+            while (curr != NULL)
+            {
+
+                if (curr->left == NULL)
+                {
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+                else
+                {
+
+                    TreeNode *prev = curr->left;
+
+                    while (prev->right != NULL && prev->right != curr)
+                        prev = prev->right;
+
+                    if (prev->right == NULL)
+                    {
+                        prev->right = curr;
+                        curr = curr->left;
+                    }
+                    else
+                    {
+                        prev->right = NULL;
+                        ans.push_back(curr->val);
+                        curr = curr->right;
+                    }
+                }
+            }
             return ans;
         }
-
-    void Traverse(TreeNode *root)
-    {
-        if (root == NULL) return;
-        Traverse(root->left);
-        ans.push_back(root->val);
-        Traverse(root->right);
-    }
 };
