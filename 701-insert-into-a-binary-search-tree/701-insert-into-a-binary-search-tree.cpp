1d5c581de3e289b1434797ab5c24e8947399fbdf
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-21 09:16:36 +0530

    Time: 201 ms (8.22%), Space: 57 MB (48.42%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -15,22 +15,33 @@ class Solution
         TreeNode* insertIntoBST(TreeNode *root, int val)
         {
 
-            return Traverse(root, val);
+            if (root == NULL)
+                return new TreeNode(val);
+
+            TreeNode *curr = root, *prev = NULL;
+
+            while (curr != NULL)
+            {
+                prev = curr;
+                if (curr->val > val)
+                {
+
+                    curr = curr->left;
+                }
+                else
+                {
+                    curr = curr->right;
+                }
+
+                if (curr == NULL)
+                {
+                    if (prev->val > val)
+                        prev->left = new TreeNode(val);
+                    else
+                        prev->right=new TreeNode(val);
+                }
+            }
+            
+            return root;
         }
-
-    TreeNode* Traverse(TreeNode *root, int val)
-    {
-        if (root == NULL)
-        {
-            return new TreeNode(val);
-        }
-
-        if (root->val > val)
-           root->left= Traverse(root->left, val);
-
-        else
-            root->right= Traverse(root->right, val);
-        
-        return root;
-    }
 };
