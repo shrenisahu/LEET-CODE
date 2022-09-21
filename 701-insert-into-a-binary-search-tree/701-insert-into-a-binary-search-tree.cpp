eeb9890445aa628306aa31b9fe16cc3d647f3c65
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-21 09:07:49 +0530

    Time: 195 ms (11.78%), Space: 56.8 MB (81.24%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -1,45 +1,36 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        TreeNode* insertIntoBST(TreeNode *root, int val) {
-            if(root==NULL)
-                return new TreeNode (val);
-            Traverse(root,val);
-            return root;
+        TreeNode* insertIntoBST(TreeNode *root, int val)
+        {
+
+            return Traverse(root, val);
         }
 
-
-void Traverse(TreeNode *root, int val)
-{
-    if (root->left == NULL && (root->val > val))
+    TreeNode* Traverse(TreeNode *root, int val)
     {
-        root->left = new TreeNode(val);
-        return;
+        if (root == NULL)
+        {
+            return new TreeNode(val);
+        }
+
+        if (root->val > val)
+           root->left= Traverse(root->left, val);
+
+        else
+            root->right= Traverse(root->right, val);
+        
+        return root;
     }
-    if (root->right == NULL && (root->val < val))
-    {
-        root->right = new TreeNode(val);
-        return;
-    }
-
-    if (root->val > val)
-        Traverse(root->left ,val);
-
-    if (root->val < val)
-        Traverse(root->right ,val);
-
-    return;
-}
-    
-    };
+};
