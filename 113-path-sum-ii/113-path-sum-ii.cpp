203fd575d7a9e9a34edfcad9fd0e0e261e8800ae
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-24 10:51:47 +0530

    Time: 22 ms (49.15%), Space: 20 MB (49.33%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -23,7 +23,7 @@ class Solution
             return ans;
         }
 
-    void Traverse(TreeNode *root, int targetSum, int currSum, vector<int> temp)
+    void Traverse(TreeNode *root, int targetSum, int currSum, vector<int> &temp)
     {
         if (root == NULL)
             return;
@@ -31,7 +31,7 @@ class Solution
         {
             currSum += root->val;
             temp.push_back(root->val);
-             // cout<<currSum<<" ";
+          
             if (currSum == targetSum)
             {
                 ans.push_back(temp);
@@ -40,10 +40,16 @@ class Solution
         }
 
         currSum += root->val;
-        // cout<<currSum<<" ";
         temp.push_back(root->val);
-        Traverse(root->left, targetSum, currSum, temp);
-        Traverse(root->right, targetSum, currSum, temp);
+        if (root->left) {
+            Traverse(root->left, targetSum, currSum, temp);
+            temp.pop_back();
+        }
+          if (root->right) {
+            Traverse(root->right, targetSum, currSum, temp);
+            temp.pop_back();
+        }
+     
         return;
     }
 };
