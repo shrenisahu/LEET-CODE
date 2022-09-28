64a8e6c6db5a0faccf65c50900f1d25570053153
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-28 21:28:38 +0530

    Time: 12 ms (19.44%), Space: 10.8 MB (32.26%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -1,36 +1,37 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
-{
-    public:
-        ListNode* removeNthFromEnd(ListNode *head, int n)
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+       
+         ListNode *dummy=new ListNode(-1);
+        dummy->next=head;
+         ListNode *fastPtr=dummy;
+         ListNode *slowPtr=dummy;
+        
+        for(int i=1;i<=n;i++)
         {
-            if(head==NULL || head->next==NULL)
-                return NULL;
-            
-            ListNode *dummyNode = new ListNode(-1);
-            dummyNode->next = head;
-            ListNode *ptr1 = dummyNode;
-            ListNode *ptr2 = dummyNode;
-            for (int i = 1; i <= n; i++)
-            {
-                ptr1 = ptr1->next;
-            }
-            while (ptr1->next != NULL)
-            {
-                ptr2 = ptr2->next;
-                ptr1 = ptr1->next;
-            }
-            ptr2->next = ptr2->next->next;
-
-            return dummyNode->next;
+            fastPtr=fastPtr->next;
         }
+            
+            
+        while(fastPtr->next!=NULL )
+        {
+            fastPtr=fastPtr->next;
+            slowPtr=slowPtr->next;
+        }
+        
+        slowPtr->next=slowPtr->next->next;
+        return dummy->next;
+        
+    }
 };
