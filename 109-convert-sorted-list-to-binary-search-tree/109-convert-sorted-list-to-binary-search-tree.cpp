35e2c5d419928df29dff511ca4ece4066051a9d4
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-09-29 06:03:45 +0530

    Time: 64 ms (15.05%), Space: 28.4 MB (40.47%) - LeetHub

---
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,71 @@
+/**
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *  int val;
+ *  ListNode * next;
+ *  ListNode() : val(0), next(nullptr) {}
+ *  ListNode(int x) : val(x), next(nullptr) {}
+ *  ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
+ */
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
+ */
+class Solution
+{
+    public:
+        TreeNode* sortedListToBST(ListNode *head) {
+            
+            return buildtree(head);
+        }
+
+    TreeNode* buildtree(ListNode *head)
+    {
+
+        if (head == NULL)
+            return NULL;
+        if (head->next == NULL)
+        {
+
+            TreeNode *temp = new TreeNode(head->val);
+            return temp;
+        }
+
+        ListNode *prev = head;
+        ListNode *fastPtr = head;
+        ListNode *slowPtr = head;
+
+        while (fastPtr->next != NULL && fastPtr->next->next)
+
+        {
+            prev = slowPtr;
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next->next;
+        }
+        TreeNode *root = new TreeNode(slowPtr->val);
+
+        ListNode *after = slowPtr->next;
+        slowPtr->next = NULL;
+        prev->next = NULL;
+
+        if (prev == slowPtr)
+        {
+            root->left = NULL;
+        }
+        else
+        {
+            root->left = buildtree(head);
+        }
+        root->right = buildtree(after);
+        
+        return root;
+    }
+};
