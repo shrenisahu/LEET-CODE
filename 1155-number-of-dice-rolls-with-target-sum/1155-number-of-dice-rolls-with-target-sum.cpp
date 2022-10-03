6d684c227d14e691833a88a3b78c8ba201abd2a4
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-10-03 10:27:32 +0530

    Time: 2702 ms (5.08%), Space: 20.8 MB (5.04%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,37 +1,30 @@
-class Solution {
-public:
-    int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-        return totalWays(n,k,target,dp);
-        
-    }
-    
-    int totalWays(int n,int f,int targetSum, vector<vector<int>>&dp)
-    {
-        
-        
-        
-        if(n==0 && targetSum==0)
-        return 1 ;
-        
-        if(n==0 || targetSum<=0)
-        return 0;    
-        
-        if(dp[n][targetSum]!=-1)
-            return dp[n][targetSum];
-        
-        int tempAns=0;
-        int ways=0;
-        int mod=1000000007;
-        for(int i=1;i<=f;i++)
+class Solution
+{
+    int mod = 1e9 + 7;
+    unordered_map<string, int> mpp;
+    public:
+        int numRollsToTarget(int n, int k, int target)
         {
-            tempAns=totalWays(n-1,f,targetSum-i,dp)%mod;
-            // ways=ways%mod;
-            // ways=(ways+tempAns)%mod;
-            
-            
-            ways=(ways+tempAns)%mod;
+            // memset(dp, -1, sizeof(dp));
+            return findCount(n, k, target);
         }
-        return  dp[n][targetSum]=ways;
+
+    int findCount(int n, int k, int target)
+    {
+        if (target == 0 && n == 0)
+            return 1;
+        if (target == 0 || n == 0)
+            return 0;
+        string key = to_string(n) + '-' + to_string(k) + '-' + to_string(target);
+        if(mpp.find(key)!=mpp.end())
+            return mpp[key];
+        int ways = 0;
+        for (int i = 1; i <= k; i++)
+        {
+            int temp = findCount(n - 1, k, target - i);
+            ways = (ways % mod + temp % mod) % mod;
+        }
+
+        return mpp[key]= ways;
     }
 };
