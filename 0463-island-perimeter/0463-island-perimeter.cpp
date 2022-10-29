a73fabdefce604f4952b912ccd0d6df9c260569a
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-10-29 19:41:38 +0530

    Time: 253 ms (24.46%), Space: 96.2 MB (85.25%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -3,60 +3,31 @@ class Solution
     public:
         int islandPerimeter(vector<vector < int>> &grid)
         {
+
             int tr = grid.size();
-            int tc = grid[0].size();
-            int ans = 0;
-            set<pair<int, int>> stt;
+            int tc  = grid[0].size();
+
+            int res = 0;
             for (int i = 0; i < tr; i++)
             {
                 for (int j = 0; j < tc; j++)
                 {
                     if (grid[i][j] == 1)
                     {
-                        DFS(i, j, tr, tc, grid, ans, stt);
-                        return ans;
+
+                        if (i == 0 || grid[i - 1][j] == 0)
+                            res += 1;
+
+                        if (i == tr - 1 || grid[i + 1][j] == 0)
+                            res += 1;
+
+                        if (j == 0 || grid[i][j - 1] == 0)
+                            res += 1;
+                        if (j == tc - 1 || grid[i][j + 1] == 0)
+                            res += 1;
                     }
                 }
             }
-
-            return ans;
+            return res;
         }
-
-    void DFS(int cr, int cc, int tr, int tc, vector<vector < int>> &grid, int &ans, set< pair<int, int>> &stt)
-    {
-
-        if (cr < 0 || cc < 0 || cr >= tr || cc >= tc || grid[cr][cc] == 0 || stt.find({ cr,
-                cc }) != stt.end())
-            return;
-
-        stt.insert({ cr,
-            cc });
-        int count = 0;
-        if (cr < tr - 1 && grid[cr + 1][cc] == 1)
-        {
-
-            count++;
-        }
-        if (cr > 0 && grid[cr - 1][cc] == 1)
-        {
-
-            count++;
-        }
-        if (cc < tc - 1 && grid[cr][cc + 1] == 1)
-        {
-
-            count++;
-        }
-        if (cc > 0 && grid[cr][cc - 1] == 1)
-        {
-
-            count++;
-        }
-
-        DFS(cr + 1, cc, tr, tc, grid, ans, stt);
-        DFS(cr - 1, cc, tr, tc, grid, ans, stt);
-        DFS(cr, cc + 1, tr, tc, grid, ans, stt);
-        DFS(cr, cc - 1, tr, tc, grid, ans, stt);
-        ans += (4 - count);
-    }
 };
