4c28e94165aa0b52aa6f2db4f4c03ed6f6020b5a
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-10-29 18:59:04 +0530

    Time: 414 ms (5.04%), Space: 131.2 MB (5.13%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+    public:
+        int islandPerimeter(vector<vector < int>> &grid)
+        {
+            int tr = grid.size();
+            int tc = grid[0].size();
+            int ans = 0;
+            set<pair<int, int>> stt;
+            for (int i = 0; i < tr; i++)
+            {
+                for (int j = 0; j < tc; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        DFS(i, j, tr, tc, grid, ans, stt);
+                        return ans;
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+    void DFS(int cr, int cc, int tr, int tc, vector<vector < int>> &grid, int &ans, set< pair<int, int>> &stt)
+    {
+
+        if (cr < 0 || cc < 0 || cr >= tr || cc >= tc || grid[cr][cc] == 0 || stt.find({ cr,
+                cc }) != stt.end())
+            return;
+
+        stt.insert({ cr,
+            cc });
+        int count = 0;
+        if (cr < tr - 1 && grid[cr + 1][cc] == 1)
+        {
+            DFS(cr + 1, cc, tr, tc, grid, ans, stt);
+            count++;
+        }
+        if ( cr > 0 && grid[cr - 1][cc] == 1)
+        {
+            DFS(cr - 1, cc, tr, tc, grid, ans, stt);
+            count++;
+        }
+        if (cc < tc - 1  && grid[cr][cc + 1] == 1)
+        {
+            DFS(cr, cc + 1, tr, tc, grid, ans, stt);
+            count++;
+        }
+        if ( cc > 0 && grid[cr][cc - 1] == 1)
+        {
+            DFS(cr, cc - 1, tr, tc, grid, ans, stt);
+            count++;
+        }
+
+        ans += (4- count);
+    }
+};
