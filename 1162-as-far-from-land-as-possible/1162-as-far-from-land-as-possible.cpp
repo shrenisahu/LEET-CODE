96652637b65a8669229562051a735705d188eda3
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-10-31 14:59:56 +0530

    Time: 513 ms (9.90%), Space: 102.8 MB (5.03%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,72 +1,84 @@
 class Solution
 {
     public:
-    
-        vector<vector<int>>dir{{1,0},{-1,0},{0,1},{0,-1}};
-    
-        int maxDistance(vector<vector < int>> &grid)
+
+        vector<vector < int>> dir
         {
-            int tr = grid.size();
-            int tc = grid[0].size();
-            int maxD=0;
-            vector<vector<int>>visited(tr,vector<int>(tc,0));
-            queue<vector < int>> que;
-            for (int i = 0; i < tr; i++)
-            {
-                for (int j = 0; j < tc; j++)
-                {
-                    if (grid[i][j] == 1)
-                    {
-                        vector<int> temp
-                        {
-                            i,
-                            j,
-                            0
-                        };
-                        que.push(temp);
-                    }
-                }
-            }
+            { 1,
+                0 },
+            { -1,
+                0 },
+            { 0,
+                1 },
+            { 0,
+                -1 }
+        };
 
-            while (!que.empty())
-            {
-                vector<int> arr = que.front();
-                que.pop();
-
-                int row = arr[0];
-                int col = arr[1];
-                int dist = arr[2];
-                
-                if(visited[row][col]==1)
-                    continue;
-                visited[row][col]=1;
-                maxD=max(maxD,dist);
-                
-                for(int i=0;i<=3 ;i++)
-                {
-                    int newRow=row+dir[i][0];
-                    int newCol=col+dir[i][1];
-                    
-                    if(isValid(newRow,newCol,tr,tc,grid,visited))
-                    {
-                        vector<int>tempArray{newRow,newCol,dist+1};
-                        que.push(tempArray);
-                    }
-                }
-                
-                
-                
-            }
-            if(maxD==0)
-                return -1;
-            return maxD;
-        }
-    
-    bool isValid(int cr,int cc,int tr,int tc,vector<vector < int>> &grid , vector<vector<int>>&visited)
+    int maxDistance(vector<vector < int>> &grid)
     {
-        if(cr<0 || cc<0 || cr>=tr || cc >=tc || grid[cr][cc]==1 || visited[cr][cc]==1)
-            return false ;
+        int tr = grid.size();
+        int tc = grid[0].size();
+        int maxD = 0;
+       
+        queue<vector < int>> que;
+        for (int i = 0; i < tr; i++)
+        {
+            for (int j = 0; j < tc; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    vector<int> temp
+                    {
+                        i,
+                        j,
+                        0
+                    };
+                    que.push(temp);
+                }
+            }
+        }
+
+        while (!que.empty())
+        {
+            vector<int> arr = que.front();
+            que.pop();
+
+            int row = arr[0];
+            int col = arr[1];
+            int dist = arr[2];
+
+            if (grid[row][col] == -1)
+                continue;
+            grid[row][col]=-1;
+            
+            maxD = max(maxD, dist);
+
+            for (int i = 0; i <= 3; i++)
+            {
+                int newRow = row + dir[i][0];
+                int newCol = col + dir[i][1];
+
+                if (isValid(newRow, newCol, tr, tc, grid))
+                {
+                    vector<int> tempArray
+                    {
+                        newRow,
+                        newCol,
+                        dist + 1
+                    };
+                    que.push(tempArray);
+                }
+            }
+        }
+        if (maxD == 0)
+            return -1;
+        return maxD;
+    }
+
+    bool isValid(int cr, int cc, int tr, int tc, vector<vector < int>> &grid)
+    {
+        if (cr < 0 || cc < 0 || cr >= tr || cc >= tc || grid[cr][cc] != 0 )
+            return false;
         return true;
-        
     }
 };
