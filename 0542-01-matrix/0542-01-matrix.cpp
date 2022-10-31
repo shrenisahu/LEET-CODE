d2038c8ca7d922f46f9b958fce209e7f54793307
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-10-31 10:17:56 +0530

    Time: 1199 ms (5.00%), Space: 156.6 MB (5.02%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,3 +1,7 @@
+// here if we start BFS from  all one simultaneously so when we reach a zero we will not be able to track for which one the answer is ,so its beeter to start with zero,if we start from 0 we dont need to track for which 0 we started beacuse our aim is to reach 1
+
+// TC:O(M*N)
+
 class Solution
 {
     public:
@@ -17,8 +21,8 @@ class Solution
 
         int tr = mat.size();
         int tc = mat[0].size();
-        vector<vector < int>> visited(tr, vector<int> (tc, 0));
-        vector<vector < int>> dist(tr, vector<int> (tc, 0));
+
+        vector<vector < int>> dist(tr, vector<int> (tc, -1));
 
         queue<vector < int>> que;
         for (int i = 0; i < tr; i++)
@@ -38,30 +42,29 @@ class Solution
 
         while (!que.empty())
         {
-            
 
             vector<int> arr = que.front();
             que.pop();
             int row = arr[0];
             int col = arr[1];
             int dis = arr[2];
-            // cout<<dis<<" ,";
-            if (visited[row][col] == 1)
+
+            if (dist[row][col] != -1)
                 continue;
-            visited[row][col] = 1;
+
             dist[row][col] = dis;
 
             for (int i = 0; i <= 3; i++)
             {
                 int newRow = row + dir[i][0];
                 int newCol = col + dir[i][1];
-                if (isValid(newRow, newCol, tr, tc, visited))
+                if (isValid(newRow, newCol, tr, tc, dist))
                 {
                     vector<int> num;
                     num.push_back(newRow);
                     num.push_back(newCol);
-                    // dis++;
-                    num.push_back(dis+1);
+
+                    num.push_back(dis + 1);
                     que.push(num);
                 }
             }
@@ -70,9 +73,9 @@ class Solution
         return dist;
     }
 
-    bool isValid(int cr, int cc, int tr, int tc, vector<vector < int>> &visited)
+    bool isValid(int cr, int cc, int tr, int tc, vector<vector < int>> &dist)
     {
-        if (cr < 0 || cc < 0 || cr >= tr || cc >= tc || visited[cr][cc] == 1)
+        if (cr < 0 || cc < 0 || cr >= tr || cc >= tc || dist[cr][cc] != -1)
             return false;
         return true;
     }
