4d8398e4763c2bec85fb861d4a720842538d15ba
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-11-05 09:57:48 +0530

    Time: 23 ms (96.47%), Space: 14 MB (18.20%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -9,34 +9,30 @@ class Solution
             {
                 if (visited[i] != -1)
                     continue;
-                bool ans = DFS(i, graph, visited,0);
+                bool ans = DFS(i, graph, visited, 0);
                 if (ans == false)
                     return false;
             }
             return true;
         }
 
-   bool DFS(int vertex,vector<vector < int>> &graph,vector<int>&visited,int currColor)
-   {
+    bool DFS(int vertex, vector<vector < int>> &graph, vector< int > &visited, int currColor)
+    {
+        if (visited[vertex]!=-1)
+        {
 
-       visited[vertex]=currColor;
-       
-       vector<int>neigh=graph[vertex];
-       for(auto eachEdge:neigh)
-       {
-           if(visited[eachEdge]==-1)
-           {
-              if( DFS(eachEdge,graph,visited,1-currColor)== false)
-                  return false;
-           }
-           else
-           {
-               if(visited[eachEdge]==currColor)
-                   return false;
-           }
-           
-          
-       }
+            return (visited[vertex] == currColor);
+                
+        }
+        visited[vertex] = currColor;
+
+        vector<int> neigh = graph[vertex];
+        for (auto eachEdge: neigh)
+        {
+
+            if (DFS(eachEdge, graph, visited, 1 - currColor) == false)
+                return false;
+        }
         return true;
-   }
+    }
 };
