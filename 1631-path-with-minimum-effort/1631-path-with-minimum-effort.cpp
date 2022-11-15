88d6c4a8d5a381920530d7d3971ae43a434535ac
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-11-15 10:40:51 +0530

    Time: 178 ms (91.79%), Space: 20.1 MB (56.28%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,62 @@
+class Solution
+{
+    private:
+    bool isValid(int cr,int cc,int tr,int tc ,vector<vector < int>> &visited)
+    {
+        if(cr<0 || cc <0 || cr >=tr || cc >=tc || visited[cr][cc]==-1)
+            return false;
+        
+        return true;
+    }
+    
+    public:
+    vector<vector<int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
+    int minimumEffortPath(vector<vector < int>> &heights)
+        {
+
+            int m = heights.size();
+            int n = heights[0].size();
+        // vector<vector < int>> visited(m,vector<int>(n,0));
+            typedef pair<int, pair<int, int>> pi;
+            priority_queue<pi, vector < pi>, greater < pi>> que;
+
+            que.push({ 0,
+                { 0,
+                    0 } });
+
+            while (!que.empty())
+            {
+                int wt = que.top().first;
+                auto currPair = que.top().second;
+                que.pop();
+
+                int cr = currPair.first;
+                int cc = currPair.second;
+                // visited[cr][cc]=1;
+                if(heights[cr][cc]==-1)
+                    continue;
+                if (cr == m - 1 && cc == n - 1)
+                    return wt;
+
+                for (int i = 0; i <= 3; i++)
+                {
+                    int nr = cr + dir[i][0];
+                    int nc = cc + dir[i][1];
+                    if (isValid(nr,nc,m,n,heights))
+                    {
+                        int cdiff = abs(heights[cr][cc] - heights[nr][nc]);
+                        int diff = max(cdiff, wt);
+                        que.push({ diff,
+                            {
+                                nr,
+                                nc
+                            } });
+                    }
+                }
+                
+                heights[cr][cc]=-1;
+            }
+
+            return 0;
+        }
+};
