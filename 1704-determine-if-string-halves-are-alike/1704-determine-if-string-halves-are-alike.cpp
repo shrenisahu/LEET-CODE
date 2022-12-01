c07beb2b307e4c979581235373a679fb840fb038
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2022-12-01 20:06:43 +0530

    Time: 60 ms (5.65%), Space: 6.5 MB (83.41%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -11,18 +11,16 @@ class Solution
             for (int i = 0; i < n / 2; i++)
             {
                 char letter = s[i];
+                char letter2 = s[n - i -1];
+                
+                cout<<letter<<" ,"<<letter2<<endl;
                 if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'A' || letter == 'I' || letter == 'O' || letter == 'E' || letter == 'U')
                     count1++;
 
-            }
-             for (int i = n/2; i < n ; i++)
-            {
-                char letter = s[i];
-                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'A' || letter == 'I' || letter == 'O' || letter == 'E' || letter == 'U')
+                if (letter2 == 'a' || letter2 == 'e' || letter2 == 'i' || letter2 == 'o' || letter2 == 'u' || letter2 == 'A' || letter2 == 'I' || letter2 == 'O' || letter2 == 'E' || letter2 == 'U')
                     count2++;
-
             }
-            
-            return count1==count2;
+
+            return count1 == count2;
         }
 };
