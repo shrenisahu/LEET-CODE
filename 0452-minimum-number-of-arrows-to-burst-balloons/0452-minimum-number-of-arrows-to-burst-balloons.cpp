7530ea2f70cc828c031869d893c307c3705b4181
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-01-06 10:51:39 +0530

    Time: 1270 ms (7.14%), Space: 102.8 MB (5.10%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,15 +1,35 @@
-bool cmp(vector<int>& a, vector<int>& b) {return a[1] < b[1];}
-class Solution {
-public:  
-    int findMinArrowShots(vector<vector<int>>& segments) {
-        sort(segments.begin(), segments.end(), cmp);
-        int ans = 0, arrow = 0;
-        for (int i = 0; i < segments.size(); i ++) {
-            if (ans == 0 || segments[i][0] > arrow) {
-                ans ++;
-                arrow = segments[i][1];
+class Solution
+{
+    public:
+        int findMinArrowShots(vector<vector < int>> &points)
+        {
+            typedef pair<int, int> pi;
+            sort(points.begin(), points.end());
+            priority_queue<pi> heap;
+            heap.push({ points[0][0],
+                points[0][1] });
+
+            for (int i = 1; i < points.size(); i++)
+            {
+                vector<int> it = points[i];
+                int x = it[0];
+                int y = it[1];
+
+                auto currPair = heap.top();
+                int x1 = currPair.first;
+                int y1 = currPair.second;
+                if (x > y1)
+                    heap.push({ x,
+                        y });
+                else
+                {
+                    heap.pop();
+                    int newCord=min(y,y1);
+                    heap.push({ x,
+                        newCord});
+                }
             }
+
+            return heap.size();
         }
-        return ans;
-    }
 };
