557ed63324722f8664ce7d60939ba69bbcc96d5a
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-01-06 15:53:56 +0530

    Time: 99 ms (17.12%), Space: 20 MB (27.52%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,44 +3,37 @@ class Solution
     public:
         vector<vector < int>> merge(vector<vector < int>> &intervals)
         {
-          typedef  pair<int,int>pi;
+            typedef pair<int, int> pi;
             vector<vector < int>> ans;
             sort(intervals.begin(), intervals.end());
-            priority_queue<pi> heap;
 
-            heap.push({ intervals[0][0],
-                intervals[0][1] });
+            int lastX = intervals[0][0];
+            int lastY = intervals[0][1];
 
             for (int i = 1; i < intervals.size(); i++)
             {
                 vector<int> it = intervals[i];
                 int x = it[0];
                 int y = it[1];
-                
-                auto currPair=heap.top();
-                int x1=currPair.first;
-                int y1=currPair.second;
-                if(x >y1)
+
+                if (x > lastY)
                 {
-                    heap.push({x,y});
+                    ans.push_back({ lastX,
+                        lastY });
+
+                    lastX = x;
+                    lastY = y;
                 }
                 else
                 {
-                    heap.pop();
-                    int newXCord=min(x,x1);
-                    int newYCord=max(y,y1);
-                    heap.push({newXCord,newYCord});
-                    
+
+                    lastX = min(x, lastX);
+                    lastY = max(y, lastY);
                 }
             }
-            
-            
-            while(heap.size()>0)
-            {
-                auto currPair=heap.top();
-                heap.pop();
-                ans.push_back({currPair.first,currPair.second});
-            }
+            ans.push_back({ lastX,
+                lastY });
+
             return ans;
         }
 };
