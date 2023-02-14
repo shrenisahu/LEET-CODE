2ff4efbbd510914fbd031e5c77d84eaeff1b4124
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-14 09:50:32 +0530

    Time: 3 ms (39.82%), Space: 6.3 MB (18.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,23 +4,23 @@ class Solution
 
         int countWays(int steps, vector<int> &dp)
         {
-            if (steps <=1)
-                return dp[steps]=1;
+            if (steps <= 0)
+                return dp[steps] = 1;
             if (dp[steps] != -1)
                 return dp[steps];
-
+            int twoSteps = 0;
             int oneStep = countWays(steps - 1, dp);
+            if (steps - 2 >= 0)
+                twoSteps = countWays(steps - 2, dp);
 
-            int twoSteps = countWays(steps - 2, dp);
-            
-            int ans=0;
-            for(int i=1;i<=2;i++)
-            {
-                int temp=countWays(steps - i, dp);
-                ans+=temp;
-            }
-
-            return dp[steps] = ans;
+           	// int ans=0;
+           	// for(int i=1;i<=2;i++)
+           	// {
+           	//     int temp=countWays(steps - i, dp);
+           	//     ans+=temp;
+           	// }
+            return dp[steps] = oneStep + twoSteps;
+           	// return dp[steps] = ans;
         }
     int climbStairs(int n)
     {
