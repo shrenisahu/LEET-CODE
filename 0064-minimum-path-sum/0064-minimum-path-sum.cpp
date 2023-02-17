5789d39d76451714b1b0fe66b1dbb7300686f844
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-17 17:16:19 +0530

    Time: 17 ms (18.08%), Space: 10.5 MB (6.72%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,31 +1,60 @@
 class Solution
 {
     public:
-        int memoization(int cr, int cc, int tr, int tc, vector<vector < int>> &grid,    vector<vector<int>>&dp)
+        int memoization(int cr, int cc, int tr, int tc, vector<vector < int>> &grid, vector< vector< int>> &dp)
         {
 
             if (cr >= tr || cc >= tc)
                 return 10000;
 
             if (cr == tr - 1 && cc == tc - 1)
-                return dp[cr][cc]= grid[cr][cc];
-            if(dp[cr][cc]!=-1)
-                  return dp[cr][cc];
+                return dp[cr][cc] = grid[cr][cc];
+            if (dp[cr][cc] != -1)
+                return dp[cr][cc];
 
-            long long down = grid[cr][cc] + memoization(cr + 1, cc, tr, tc, grid,dp);
-            long long left = grid[cr][cc] + memoization(cr, cc + 1, tr, tc, grid,dp);
-            
-            return dp[cr][cc]=min(down,left);
+            long long down = grid[cr][cc] + memoization(cr + 1, cc, tr, tc, grid, dp);
+            long long left = grid[cr][cc] + memoization(cr, cc + 1, tr, tc, grid, dp);
+
+            return dp[cr][cc] = min(down, left);
         }
 
-    int minPathSum(vector<vector < int>> &grid) {
-        
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n,-1));
-      
-        int ans=memoization(0,0,m,n,grid,dp);
-        
+    int tabulation(vector<vector < int>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector < int>> dp(m + 1, vector<int> (n + 1, 1000));
+
+       	// dp[m - 1][n - 1] = grid[m - 1][n - 1];
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+
+                if (i == m - 1 && j == n - 1)
+                    dp[i][j] = grid[i][j];
+
+                else
+                {
+                    int down = grid[i][j] + dp[i + 1][j];
+                    int right = grid[i][j] + dp[i][j + 1];
+
+                    dp[i][j] = min(down, right);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    int minPathSum(vector<vector < int>> &grid)
+    {
+
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector < int>> dp(m + 1, vector<int> (n, -1));
+
+       	// int ans = memoization(0, 0, m, n, grid, dp);
+        int ans = tabulation(grid);
         return ans;
     }
 };
