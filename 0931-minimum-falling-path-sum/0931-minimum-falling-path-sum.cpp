07a27fc6f2d29bda8e1fe1a71c036db3a40c0bd4
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-18 18:49:22 +0530

    Time: 18 ms (48.85%), Space: 10.8 MB (5.23%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -8,8 +8,8 @@ class Solution
             if (cc < 0 || cc >= tr)
                 return 100000;
 
-            if (cr >= tr)
-                return dp[cr][cc] = 0;
+            if (cr == tr - 1)
+                return dp[cr][cc] = matrix[cr][cc];
 
             if (dp[cr][cc] != -1)
                 return dp[cr][cc];
@@ -27,18 +27,50 @@ class Solution
             return dp[cr][cc] = ans;
         }
 
+    int tabulation(vector<vector < int>> &matrix)
+    {
+        int n = matrix.size();
+        vector<vector < int>> dp(n + 1, vector<int> (n + 1, 0));
+
+        for (int j = n - 1; j >= 0; j--)
+        {
+
+            for (int k = n - 1; k >= 0; k--)
+            {
+                int ld = 100000, rd = 100000, d = 100000;
+                if (k - 1 >= 0)
+                    ld = dp[j + 1][k - 1];
+                d = dp[j + 1][k];
+                if (k + 1 < n)
+                    rd = dp[j + 1][k + 1];
+
+                dp[j][k] = matrix[j][k] + min({ ld,
+                    rd,
+                    d });
+            }
+        }
+        int minAns = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            minAns = min(minAns, dp[0][i]);
+        }
+
+        return minAns;
+    }
+
     int minFallingPathSum(vector<vector < int>> &matrix)
     {
         int n = matrix.size();
         vector<vector < int>> dp(n + 1, vector<int> (n + 1, -1));
 
-        int minAns = INT_MAX;
-        for (int i = 0; i < n; i++)
-        {
-            int ans = memoization(0, i, n, matrix, dp);
-            minAns = min(ans, minAns);
-        }
-
-        return minAns;
+       	// int minAns = INT_MAX;
+       	// for (int i = 0; i < n; i++)
+       	// {
+       	//     int ans = memoization(0, i, n, matrix, dp);
+       	//     minAns = min(ans, minAns);
+       	// }
+        int ans = tabulation(matrix);
+        return ans;
+       	// return minAns;
     }
 };
