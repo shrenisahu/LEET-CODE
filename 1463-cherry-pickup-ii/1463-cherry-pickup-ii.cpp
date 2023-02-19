3036b139e04031b7169b3c704857c971a6c34bce
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-19 15:47:48 +0530

    Time: 269 ms (17.47%), Space: 26.2 MB (5.86%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -9,7 +9,7 @@ class Solution
 
             if (r == grid.size())
                 return 0;
-            if(dp[r][c1][c2]!=-1)
+            if (dp[r][c1][c2] != -1)
                 return dp[r][c1][c2];
             int maxi = -1000;
 
@@ -35,8 +35,53 @@ class Solution
                 }
             }
 
-            return dp[r][c1][c2]=maxi;
+            return dp[r][c1][c2] = maxi;
         }
+
+    int Tabulation(vector<vector < int>> &grid)
+    {
+        int tr = grid.size();
+        int tc = grid[0].size();
+        vector<vector<vector< int>>> dp(tr + 1, vector<vector < int>> (tc + 1, vector<int> (tc + 1, 0)));
+
+        for (int r = tr - 1; r >= 0; r--)
+        {
+            for (int c1 = 0; c1 < tc; c1++)
+            {
+                for (int c2 = 0; c2 < tc; c2++) {
+                     int maxi = -1000;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int temp = -10000;
+                    if (c1 + i >= 0 && c2 + j >= 0 && c1 + i < tc && c2 + j < tc)
+                    {
+                        int val = 0;
+                        if (c1 == c2)
+                        {
+                            val = grid[r][c1];
+                        }
+                        else
+                        {
+                            val = grid[r][c1] + grid[r][c2];
+                        }
+                        temp = val + dp[r+1][c1+i][c2+j];
+                        
+                    }
+                    maxi = max(temp, maxi);
+                }
+            }
+
+           dp[r][c1][c2] = maxi;
+                    
+                }
+            }
+        }
+        
+        return dp[0][0][tc-1];
+    }
 
     int cherryPickup(vector<vector < int>> &grid)
     {
@@ -45,7 +90,8 @@ class Solution
         int tc = grid[0].size();
         vector<vector<vector< int>>> dp(tr + 1, vector<vector < int>> (tc + 1, vector<int> (tc + 1, -1)));
 
-        int ans = Solve(0, 0, tc - 1, tc, grid, dp);
+        // int ans = Solve(0, 0, tc - 1, tc, grid, dp);
+        int ans=Tabulation(grid);
         return ans;
     }
 };
