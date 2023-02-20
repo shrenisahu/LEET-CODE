eb811ea9eb7757e9868c4c5ef553f8113f2efafd
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-20 23:11:40 +0530

    Time: 422 ms (33.78%), Space: 95.9 MB (19.05%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+    public:
+
+        bool Solve(int idx, int currSum, int targetSum, vector<int> &nums, vector<vector< int>> &dp)
+        {
+
+            if (currSum == targetSum)
+                return true;
+
+            if (idx >= nums.size()) return false;
+
+            if (dp[idx][currSum] != -1)
+                return dp[idx][currSum];
+
+            bool notTake = Solve(idx + 1, currSum, targetSum, nums, dp);
+            bool take = false;
+
+            if (nums[idx] + currSum <= targetSum)
+                take = Solve(idx + 1, nums[idx] + currSum, targetSum, nums, dp);
+            return dp[idx][currSum] = take || notTake;
+        }
+
+    bool canPartition(vector<int> &nums)
+    {
+        int n = nums.size();
+        int sum = 0;
+        for (auto &i: nums)
+            sum += i;
+
+        if (sum % 2 == 1)
+            return false;
+        int targetSum = sum / 2;
+
+        vector<vector < int>> dp(n + 1, vector<int> (targetSum + 1, -1));
+        bool ans = Solve(0, 0, targetSum, nums, dp);
+
+        return ans;
+    }
+};
+
+// Input: nums =[1,2,3,5]
+// Output: false
+// Explanation: The array cannot be partitioned into equal sum subsets.
