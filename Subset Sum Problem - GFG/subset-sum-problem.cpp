477a7cabdb2581a47324c00aab80da58adcef775
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-22 23:02:13 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -10,38 +10,57 @@ using namespace std;
 class Solution{   
 public:
 
-bool Tabulate(int n, int sum, vector<int> &arr)
+bool Solve(int idx,int sum,int target,vector<int>arr, vector<vector<int>>&dp)
 {
-     vector<vector<int>>dp(n+1,vector<int>(sum+10));
-
-     for(int i=0;i<=n;i++)
-     dp[i][sum]=1;
-     int last=arr[n-1];
-     dp[n-1][last]=1;
-
     
-
-    for(int idx=n-1;idx>=0;idx--)
-    {
-        for(int curr=sum-1;curr>=0;curr--)
-        {
-            bool notTake=dp[idx+1][curr];
-            bool take=false;
-            if(curr+arr[idx]<=sum)
-            take=dp[idx+1][curr+arr[idx]];
-            dp[idx][curr]=take|notTake;
-        }
-    }
-
-    return dp[0][0];
-
-
+    
+   
+    int n=arr.size();
+     
+    if(idx==arr.size()-1)
+    return arr[n-1]==sum;
+    if(dp[idx][sum]!=-1)
+    return dp[idx][sum];
+    int take=0;
+    if(sum-arr[idx]>=0)
+    take=Solve(idx+1,sum-arr[idx],target,arr,dp);
+    
+    bool notTake=Solve(idx+1,sum,target,arr,dp);
+    
+    return dp[idx][sum]= take||notTake;
 }
 
+
+bool Tabulate(vector<int>arr, int sum)
+{
+     int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
+        
+        for(int idx=0;idx<=n;idx++)
+        dp[idx][sum]=1;
+        
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int currSum=sum;currSum>=0;currSum--)
+            {
+                int take=0;
+                if(arr[idx]+currSum<=sum)
+                take=dp[idx+1][currSum+arr[idx]];
+                
+                int notTake=dp[idx+1][currSum];
+                
+                dp[idx][currSum]=take||notTake;
+            }
+        }
+    return dp[0][0];
+}
+
+
     bool isSubsetSum(vector<int>arr, int sum){
-        // code here 
         int n=arr.size();
-        int ans=Tabulate(n,sum,arr);
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        int ans=Solve(0,sum,sum,arr,dp);
+        // int ans=Tabulate(arr,sum);
         return ans;
     }
 };
