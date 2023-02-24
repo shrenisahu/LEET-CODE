72628a79fb32f51db7ce46150b2e724fc67150bb
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-24 10:20:26 +0530

    Time: 2009 ms (5.02%), Space: 9.1 MB (85.40%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,84 +1,27 @@
-// class Solution
-// {
-//     public:
+class Solution
+{
+    public:
 
-//         int Solve(int idx, int target, vector<int> &nums, unordered_map<string, int> &mpp)
-//         {
-//             if (idx == nums.size() && target == 0)
-//                 return 1;
-//             if (idx == nums.size())
-//                 return 0;
+        int Solve(int idx, int target, vector<int> &nums)
+        {
+            if (idx == nums.size() && target == 0)
+                return 1;
+            if (idx == nums.size())
+                return 0;
 
-//             string currStr = to_string(idx) + "-" + to_string(target);
-//             if (mpp.find(currStr) != mpp.end())
-//                 return mpp[currStr];
-//             int takePlus = Solve(idx + 1, target - nums[idx], nums, mpp);
-//             int takeMinus = Solve(idx + 1, target + nums[idx], nums, mpp);
+            
+           
+            int takePlus = Solve(idx + 1, target - nums[idx], nums);
+            int takeMinus = Solve(idx + 1, target + nums[idx], nums);
 
-//             return mpp[currStr] = takePlus + takeMinus;
-//         }
-
-//     int Tabulate(vector<int> &nums, int target)
-//     {
-//         int n = nums.size();
-//         unordered_map<string, int> mpp;
-
-//         // vector<vector < int>> dp(n + 1, vector<int> (target + 1, 0));
-
-//         for (int idx = 0; idx <= n; idx++)
-//         {
-//             string currStr = to_string(idx) + "-" + to_string(0);
-//             mpp[currStr] = 1;
-//         }
-//         for (int idx = n - 1; idx >= 0; idx--)
-//         {
-//             for (int sum = 1; sum <= target; sum++)
-//             {
-//                 string currStr = to_string(idx) + "-" + to_string(sum);
-//                 string currStrTake = to_string(idx + 1) + "-" + to_string(sum - nums[idx]);
-//                 string currStrNotTake = to_string(idx + 1) + "-" + to_string(sum + nums[idx]);
-//                 int takePlus = mpp[currStrTake];
-//                 int takeMinus = mpp[currStrNotTake];
-
-//                 mpp[currStr] = takePlus + takeMinus;
-//             }
-//         }
-//         string currStr = to_string(0) + "-" + to_string(target);
-//         return mpp[currStr];
-//     }
-
-//     int findTargetSumWays(vector<int> &nums, int target)
-//     {
-//         int n = nums.size();
-//         unordered_map<string, int> mpp;
-//        	// int ans = Solve(0, target, nums, mpp);
-//         int ans = Tabulate(nums, target);
-
-//         return ans;
-//     }
-// };
-
-class Solution {
-public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return TargetSum(nums, target,0);
-    }
-    
-    
-    int TargetSum(vector<int>& nums, int target,int currIndex)
+            return takePlus + takeMinus;
+        }
+    int findTargetSumWays(vector<int> &nums, int target)
     {
-        if(currIndex==nums.size()&& target==0)
-            return 1;
-        
-        if(currIndex==nums.size()&& target!=0)
-            return 0;
-        
-        
-        int added=TargetSum(nums,target-nums[currIndex],currIndex+1);
-         int sub=TargetSum(nums,target+nums[currIndex],currIndex+1);
-        
-        return added+sub;
-        
+        int n = nums.size();
+        unordered_map<string, int> mpp;
+        int ans = Solve(0, target, nums);
+
+        return ans;
     }
 };
-
