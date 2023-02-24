6041ccb08e009eab6daeb61f1b9bb60eea90ff08
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-24 08:49:21 +0530

    Time: 175 ms (19.55%), Space: 88.2 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,55 @@
 class Solution
 {
     public:
-        int Solve(int idx, int amount, vector<vector<int>> &dp,vector<int> &coins)
+        int Solve(int idx, int amount, vector<vector < int>> &dp, vector< int > &coins)
         {
-            
-            if(amount==0)
+
+            if (amount == 0)
                 return 0;
-            
-            if(idx>=coins.size())
+
+            if (idx >= coins.size())
                 return 1e9;
-            
-            if(dp[idx][amount]!=-1)
+
+            if (dp[idx][amount] != -1)
                 return dp[idx][amount];
 
-            int notTake=Solve(idx+1,amount,dp,coins);
-            int take=1e9;
-            if(amount>=coins[idx])
-                take=1+Solve(idx,amount-coins[idx],dp,coins);
-            
-            return dp[idx][amount]= min(take,notTake);
-           
+            int notTake = Solve(idx + 1, amount, dp, coins);
+            int take = 1e9;
+            if (amount >= coins[idx])
+                take = 1 + Solve(idx, amount - coins[idx], dp, coins);
+
+            return dp[idx][amount] = min(take, notTake);
         }
 
-   
+    int Tabulate(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        vector<vector < int>> dp(n + 1, vector<int> (amount + 1, 1e9));
+        for (int idx = 0; idx <= n; idx++)
+            dp[idx][0] = 0;
+
+        for (int idx = n - 1; idx >= 0; idx--)
+        {
+            for (int sum = 1; sum <= amount; sum++)
+            {
+                int notTake = dp[idx + 1][sum];
+                int take = 1e9;
+                if (sum >= coins[idx])
+                    take = 1 + dp[idx][sum - coins[idx]];
+
+                dp[idx][sum] = min(take, notTake);
+            }
+        }
+        
+        return dp[0][amount];
+    }
 
     int coinChange(vector<int> &coins, int amount)
     {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount+1,-1));
-       	int ans=Solve(0,amount,dp,coins);
-        
+        vector<vector < int>> dp(n + 1, vector<int> (amount + 1, -1));
+        // int ans = Solve(0, amount, dp, coins);
+           int ans=Tabulate(coins,amount);
         if (ans == 1e9)
             return -1;
 
