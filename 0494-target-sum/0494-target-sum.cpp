3b294af49f4db6fa3706586c233d8f8f3ddb2ab8
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-25 08:08:52 +0530

    Time: 24 ms (59.44%), Space: 16 MB (31.00%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,22 +2,47 @@ class Solution
 {
     public:
 
-        int Solve(int idx, int target, vector<int> &nums,  vector<vector<int>>&dp)
+        int Solve(int idx, int target, vector<int> &nums, vector<vector< int>> &dp)
         {
 
-            if (idx >=nums.size())
+            if (idx >= nums.size())
             {
                 return target == 0;
             }
-            if(dp[idx][target]!=-1)
+            if (dp[idx][target] != -1)
                 return dp[idx][target];
-            int notTake = Solve(idx + 1, target, nums,dp);
+            int notTake = Solve(idx + 1, target, nums, dp);
             int take = 0;
             if (target >= nums[idx])
-                take = Solve(idx + 1, target - nums[idx], nums,dp);
+                take = Solve(idx + 1, target - nums[idx], nums, dp);
 
-            return  dp[idx][target]=take + notTake;
+            return dp[idx][target] = take + notTake;
         }
+
+    int Tabulate(vector<int> &nums, int currTarget)
+    {
+        int n = nums.size();
+        vector<vector < int>> dp(n + 2, vector<int> (currTarget + 1, 0));
+
+        for (int idx = 0; idx <= n; idx++)
+            dp[idx][0] = 1;
+
+        for (int idx = n - 1; idx >= 0; idx--)
+        {
+            for (int sum = 0; sum <= currTarget; sum++)
+            {
+                int notTake=dp[idx+1][sum];
+                int take=0;
+                if(sum>=nums[idx])
+                    take=dp[idx+1][sum-nums[idx]];
+
+                dp[idx][sum] = take + notTake;
+            }
+        }
+
+        return dp[0][currTarget];
+    }
+
     int findTargetSumWays(vector<int> &nums, int target)
     {
         int n = nums.size();
@@ -30,14 +55,15 @@ class Solution
         if ((totalSum + target) % 2 == 1)
 
             return 0;
-        
-        if(totalSum<abs(target))
-              return 0;
+
+        if (totalSum < abs(target))
+            return 0;
 
         int currTarget = (totalSum + target) / 2;
-        vector<vector<int>>dp(n+2,vector<int>(currTarget+1,-1));
-        // int ans = 0;
-       	int ans = Solve(0, currTarget, nums,dp);
+        vector<vector < int>> dp(n + 2, vector<int> (currTarget + 1, -1));
+       	// int ans = 0;
+       	// int ans = Solve(0, currTarget, nums,dp);
+        int ans = Tabulate(nums, currTarget);
         return ans;
     }
 };
