f7cc96c01df896873f836172b30dda0544f1ee50
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-02-25 07:58:14 +0530

    Time: 19 ms (66.16%), Space: 12.5 MB (36.41%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,26 +2,42 @@ class Solution
 {
     public:
 
-        int Solve(int idx, int target, vector<int> &nums)
+        int Solve(int idx, int target, vector<int> &nums,  vector<vector<int>>&dp)
         {
-            if (idx == nums.size() && target == 0)
-                return 1;
-            if (idx == nums.size())
-                return 0;
 
-            
-           
-            int takePlus = Solve(idx + 1, target - nums[idx], nums);
-            int takeMinus = Solve(idx + 1, target + nums[idx], nums);
+            if (idx >=nums.size())
+            {
+                return target == 0;
+            }
+            if(dp[idx][target]!=-1)
+                return dp[idx][target];
+            int notTake = Solve(idx + 1, target, nums,dp);
+            int take = 0;
+            if (target >= nums[idx])
+                take = Solve(idx + 1, target - nums[idx], nums,dp);
 
-            return takePlus + takeMinus;
+            return  dp[idx][target]=take + notTake;
         }
     int findTargetSumWays(vector<int> &nums, int target)
     {
         int n = nums.size();
-        unordered_map<string, int> mpp;
-        int ans = Solve(0, target, nums);
+        int totalSum = 0;
+        for (auto &i: nums)
+        {
+            totalSum += i;
+        }
 
+        if ((totalSum + target) % 2 == 1)
+
+            return 0;
+        
+        if(totalSum<abs(target))
+              return 0;
+
+        int currTarget = (totalSum + target) / 2;
+        vector<vector<int>>dp(n+2,vector<int>(currTarget+1,-1));
+        // int ans = 0;
+       	int ans = Solve(0, currTarget, nums,dp);
         return ans;
     }
 };
