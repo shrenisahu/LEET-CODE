da89f9ba5de5d9db7661c3e217720f1a8405c7e4
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-03-09 08:46:09 +0530

    Time: 92 ms (36.29%), Space: 28.1 MB (6.67%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -49,23 +49,20 @@ class Solution
         {
             dp[i][0] = 0;
         }
-        
-        
+
         for (int j = 1; j <= n; j++)
         {
             bool flag = true;
 
-            for (int k = j; k >0; k--)
+            for (int k = j; k > 0; k--)
             {
-                if (p[k-1] != '*')
+                if (p[k - 1] != '*')
                 {
                     flag = false;
                     break;
                 }
-               
-            } 
+            }
             dp[0][j] = flag;
-            
         }
 
         for (int i = 1; i <= m; i++)
@@ -73,26 +70,69 @@ class Solution
             for (int j = 1; j <= n; j++)
             {
                 if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
-                    {
-                        dp[i][j] = dp[i - 1][j - 1];
-                    }
-                  else   if (p[j-1] == '*')
-                    {
-                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                    }
-                else dp[i][j]=false;
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
                 }
+                else if (p[j - 1] == '*')
+                {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else dp[i][j] = false;
             }
-            return dp[m][n];
         }
+        return dp[m][n];
+    }
+bool isAllStars(string & S1, int i) {
 
-        bool isMatch(string s, string p)
+ 
+  for (int j = 1; j <= i; j++) {
+    if (S1[j - 1] != '*')
+      return false;
+  }
+  return true;
+}
+    bool spaceOpt(string s, string p)
+    {
+        // int m = s.size();
+        // int n = p.size();
+          int n = s.size();
+        int m = p.size();
+
+        vector<bool> dp(n + 1, 0), temp(n + 1, 0);
+        dp[0] = 1;
+      
+      
+
+        for (int i = 1; i <= m; i++)
         {
-            int m = s.size();
-            int n = p.size();
-            vector<vector < int>> dp(m + 1, vector<int> (n + 1, -1));
-            // bool ans = isValid(m, n, s, p, dp);
-            bool ans=Tabulate(s,p);
-            return ans;
+
+            temp[0] = isAllStars(p,i);
+
+
+            for (int j = 1; j <= n; j++)
+            {
+                if (s[j - 1] == p[i - 1] || p[i - 1] == '?')
+                {
+                    temp[j] = dp[j - 1];
+                }
+                else if (p[i - 1] == '*')
+                {
+                    temp[j] = dp[j] || temp[j - 1];
+                }
+                else temp[j] = false;
+            }
+            dp = temp;
         }
-    };
+        return dp[n];
+    }
+
+    bool isMatch(string s, string p)
+    {
+        int m = s.size();
+        int n = p.size();
+        vector<vector < int>> dp(m + 1, vector<int> (n + 1, -1));
+       	// bool ans = isValid(m, n, s, p, dp);
+        bool ans = spaceOpt(s, p);
+        return ans;
+    }
+};
