d6cbbc54a2613abde36b45b5d57efc1b4e3066fb
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-03-10 10:44:18 +0530

    Time: 4 ms (73.48%), Space: 20.8 MB (6.50%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -5,8 +5,8 @@ class Solution
         {
             if (pattern[j] != '*')
             {
-                cout<<"here";
-                 return false;
+                cout << "here";
+                return false;
             }
             bool isCharContinous = false;
             for (int i = j; i >= 0; i = i - 1)
@@ -27,7 +27,7 @@ class Solution
             return true;
         }
 
-    bool isValid(int i, int j, string str, string pattern,vector<vector < int>> &dp)
+    bool isValid(int i, int j, string str, string pattern, vector<vector < int>> &dp)
     {
 
         if (i == 0 && j == 0)
@@ -38,36 +38,53 @@ class Solution
 
         if (i == 0 && j > 0)
         {
-            return checkForStars(pattern, j-1);
+            return checkForStars(pattern, j - 1);
         }
 
-        if(dp[i][j]!=-1)
+        if (dp[i][j] != -1)
             return dp[i][j];
-        if (str[i-1] == pattern[j-1] || pattern[j-1] == '.')
+        if (str[i - 1] == pattern[j - 1] || pattern[j - 1] == '.')
         {
-          return dp[i][j]= isValid(i - 1, j - 1, str, pattern,dp);
+            return dp[i][j] = isValid(i - 1, j - 1, str, pattern, dp);
         }
-        else if (pattern[j-1] == '*')
+        else if (pattern[j - 1] == '*')
         {
-            bool zeroOccurence = isValid(i, j - 2, str, pattern,dp);
+            bool zeroOccurence = isValid(i, j - 2, str, pattern, dp);
             bool morOccurence = false;
-            if (pattern[j - 2] == str[i-1] || pattern[j - 2] == '.')
+            if (pattern[j - 2] == str[i - 1] || pattern[j - 2] == '.')
             {
 
-                morOccurence = isValid(i - 1, j, str, pattern,dp);
+                morOccurence = isValid(i - 1, j, str, pattern, dp);
             }
-            return dp[i][j]= zeroOccurence || morOccurence;
+            return dp[i][j] = zeroOccurence || morOccurence;
         }
         else
-            return dp[i][j]= false;
+            return dp[i][j] = false;
     }
+
+//     bool Tabulate(string str, string pattern)
+//     {
+//         int m = str.size();
+//         int n = pattern.size();
+//         vector<vector < int>> dp(m + 1, vector<int> (n + 1, 0));
+//         dp[0][0]=1;
+//         for(int i=1;i<=m;i++)
+//         {
+//             dp[i][0]=0;
+//         }
+//         for(int j=1;j<=n;j++)
+//         {
+//             dp[]
+            
+//         }
+//     }
 
     bool isMatch(string str, string pattern)
     {
         int m = str.size();
         int n = pattern.size();
         vector<vector < int>> dp(m + 1, vector<int> (n + 1, -1));
-        bool ans = isValid(m , n , str, pattern,dp);
+        bool ans = isValid(m, n, str, pattern, dp);
 
         return ans;
     }
