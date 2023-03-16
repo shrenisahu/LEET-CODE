004270e12555ddcf68396cff02d6a4b51a89b70a
shrenisahu <82773990+shrenisahu@users.noreply.github.com>
2023-03-16 16:23:26 +0530

    Time: 773 ms (22.54%), Space: 572.2 MB (9.66%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,32 +1,56 @@
-class Solution {
-public:
-    
-    int SolveLIS(int idx,int prev_idx,vector<int>& nums, vector<vector<int>>&dp)
+class Solution
+{
+    public:
+
+        int SolveLIS(int idx, int prev_idx, vector<int> &nums, vector<vector< int>> &dp)
+        {
+
+            if (idx == nums.size())
+            {
+                return 0;
+            }
+
+            if (dp[idx][prev_idx + 1] != -1)
+                return dp[idx][prev_idx + 1];
+            int notTake = SolveLIS(idx + 1, prev_idx, nums, dp);
+            int take = 0;
+
+            if (prev_idx == -1 || nums[idx] > nums[prev_idx])
+            {
+                take = 1 + SolveLIS(idx + 1, idx, nums, dp);
+            }
+
+            return dp[idx][prev_idx + 1] = max(notTake, take);
+        }
+    int Tabulate(vector<int> &nums)
     {
-        
-        if(idx==nums.size())
+        int n = nums.size();
+        vector<vector < int>> dp(n + 1, vector<int> (n + 1, 0));
+
+        for (int idx = n - 1; idx >= 0; idx--)
         {
-            return 0;
+            for (int prev_idx = idx - 1; prev_idx >= -1; prev_idx--)
+            {
+                int notTake = dp[idx + 1][prev_idx+1];
+                int take = 0;
+
+                if (prev_idx == -1 || nums[idx] > nums[prev_idx])
+                {
+                    take = 1 + dp[idx + 1][ idx+1];
+                }
+
+                 dp[idx][prev_idx + 1] = max(notTake, take);
+            }
         }
         
-        if(dp[idx][prev_idx+1]!=-1)
-            return dp[idx][prev_idx+1];
-        int notTake=SolveLIS(idx+1,prev_idx,nums,dp);
-         int take=0;
-        
-        if(prev_idx==-1||nums[idx] >nums[prev_idx])
-        {
-             take=1+SolveLIS(idx+1,idx,nums,dp);
-        }
-        
-        return dp[idx][prev_idx+1]= max(notTake,take);
+        return dp[0][0];
     }
-    
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans=SolveLIS(0,-1,nums,dp);
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector < int>> dp(n + 1, vector<int> (n + 1, -1));
+        // int ans = SolveLIS(0, -1, nums, dp);
+        int ans=Tabulate(nums);
         return ans;
-        
     }
 };
